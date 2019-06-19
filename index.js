4d2ab0e59d3f3a64a8342814de46a5a33d85a839
Fred Spencer <fred@gohunt.com>
2019-06-19 16:53:22 -0700

    Update to latest convention.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import xmldom from "xmldom";
 import resolveAssetSource from "react-native/Libraries/Image/resolveAssetSource";
 
-import { Svg } from "expo";
+import * as Svg from 'react-native-svg';
 const {
   Circle,
   Ellipse,
